9e971e171abf162444a56d4cca5a3b84ad64dc0d
Arvid Andersson <arvid@rvd.se>
2021-12-06 22:54:28 +0100

    Removed some unnecessary brackets

--- 06/aoc06.hs
+++ 06/aoc06.hs
@@ -6,5 +6,5 @@ solve input = "Problem 1: " ++ (show.sum.cycle) 80 ++ "\n"
         -- cycle n returns a list where the element at index i
         -- is the number of fish at age i after n generations.
         cycle 0 = map (\age->length (filter (\c->c==age) input)) ['0'..'8']
-        cycle n = zipWith (+) (prevTail++[0]) ([0,0,0,0,0,0,prevHead,0,prevHead])
+        cycle n = zipWith (+) (prevTail++[0]) [0,0,0,0,0,0,prevHead,0,prevHead]
             where (prevHead:prevTail) = cycle (n-1)
